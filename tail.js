00a3741bd9c4233ab41200040e4edbf627f726d8
gforsythe <gabe.forsythe@gmail.com>
2021-01-05 19:18:46 +0000

    Ive tred to push

--- tail.js
+++ tail.js
@@ -16,4 +16,4 @@ function tail(array) {
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length,3); 
+assertEqual (words.length,3); 
