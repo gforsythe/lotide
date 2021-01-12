0fd889ec692ac37424b164cc568daedb06a80b9e
gforsythe <gabe.forsythe@gmail.com>
2021-01-12 02:45:20 +0000

    shorthand added to index

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
+
+
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
 
+
+
+
+
+
+
+ 
